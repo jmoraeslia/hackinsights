5b9bec7fac11ecdace5a61de1472518c6d92e3ed
Julia Moraes <152508816+jmoraeslia@users.noreply.github.com>
2023-11-30 12:00:04 -0300

    importing hackinsights repository

---
+++ script/script.js
@@ -0,0 +1,24 @@
+
+// Frase que você deseja exibir
+const a = "Deeply explore your curiosity; learn about what excites you most.";
+
+// Obter o elemento <p> pelo ID
+const element = document.getElementById('text');
+
+// Função para exibir a frase digitando automaticamente
+function display(a, element) {
+    let index = 0;
+
+    function digitar() {
+        if (index < a.length) {
+            element.innerHTML += a.charAt(index);
+            index++;
+            setTimeout(digitar, 50); // Ajuste a velocidade da digitação aqui
+        }
+    }
+
+    digitar();
+}
+
+// Chamar a função para exibir a frase
+display(a, element);
